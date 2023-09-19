295f6ca987fbd2531b345d59c7925675aafda54d
mradovic10 <radovic.miodrag10@gmail.com>
2023-09-19 17:08:12 -0400

    Inclusion of application count and interview count in app, as well as the addition of the data.js file in static/js.

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,23 @@
 function getCurrentDateAndTime() {
     const dateTime = new Date();
     return dateTime.toLocaleString();
-}
+};
 
 // Target an HTML element to display the current date and time.
-const dateDisplay = document.getElementById("date-container");
+const dateDisplay = document.getElementById('dateContainer');
 
 // Set the innerHTML of the element to the current date and time returned by the function.
 dateDisplay.innerHTML = getCurrentDateAndTime();
+
+// Preview 'Applications' json file in console.
+console.log(applications)
+
+// Show the number of applications sent.
+const appCount = document.getElementById('appCount');
+
+appCount.innerHTML = applications.length;
+
+// Show the number of interviews that occured.
+const intCount = document.getElementById('intCount');
+
+intCount.innerHTML = interviews.length;
